0e94b216b6ba4df47c2828d76ae27fc73a6406a3
Oliver Squibb <osquibb@gmail.com>
2019-04-03 13:28:40 -0400

    initial commit

---
+++ src/components/Sequencer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Sequencer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div>
+      sequencer test
+      </div>
+    );
+  }
+}
+
+export default Sequencer;
