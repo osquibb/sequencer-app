30de19ab71cbc327a2ae431988e5bd84461ab88e
Oliver Squibb <osquibb@gmail.com>
2019-04-08 22:26:27 -0400

    custom sequencer wrapper component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
-import Sequencer from './components/Sequencer.js';
+import CustomSequencer from './components/CustomSequencer.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Container>
-        <Sequencer tracks='2' steps='8' bpm='120' />
+        <CustomSequencer />
       </Container>
     );
   }
