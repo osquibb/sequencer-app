15ac1463d92a7db2922b27a91e01f15948a9d666
Oliver Squibb <osquibb@gmail.com>
2019-05-01 10:40:15 -0400

    added modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import React, { Component, Fragment } from 'react';
+import { Container, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Sequencer from './components/Sequencer.js';
 import './App.css';
 
@@ -13,11 +13,38 @@ const soundUrls = ['https://osquibb.github.io/public/sounds/kick.mp3',
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: true
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({modal: false});
+  }
+
   render() {
     return (
-      <Container>
-        <Sequencer sounds={sounds} soundUrls={soundUrls} />
-      </Container>
+      <Fragment>
+        <Container>
+          <Sequencer sounds={sounds} soundUrls={soundUrls} />
+        </Container>
+        <Modal isOpen={this.state.modal} toggle={this.toggleModal}>
+        <ModalHeader toggle={this.toggle}>How to Play</ModalHeader>
+        <ModalBody>
+          <ol>
+            <li>Click on cells in the sequencer</li>
+            <li>Then press play!</li>
+          </ol>
+          <p>(Hint: Try adding tracks and changing the sounds!)</p>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="info" outline onClick={this.toggleModal}>Got it!</Button>
+        </ModalFooter>
+      </Modal>
+    </Fragment>
     );
   }
 }
